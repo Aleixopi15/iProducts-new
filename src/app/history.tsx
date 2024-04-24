2edbbab5a880f76b520d31ba0879ea40279ddd3f
Aleixopi15 <69539155+Aleixopi15@users.noreply.github.com>
2024-04-23 21:53:29 -0300

    Create nothing page history

--- src/app/history.tsx
+++ src/app/history.tsx
@@ -1,11 +1,27 @@
 import { View, Text, TouchableOpacity } from "react-native";
+import { MaterialIcons } from "@expo/vector-icons";
 
 import { Header } from "@/components/header";
+import { colors } from "@/styles/colors";
+import { ButtonLogin } from "@/components/button-login";
+import { Button } from "@/components/button";
+import { router } from "expo-router";
+colors
 
 export default function History() {
     return(
     <View className="flex-1 bg-gray-100 p-8 pt-16">
         <Header title="Histórico de pedidos" />
+
+        <View className="items-center justify-center">
+        <MaterialIcons className="pt-32 opacity-80" name="history" size={200} color={colors.gray[200]}/>
+        <Text className="font-inter-bold text-3xl">Nenhum pedido</Text>
+        <Text className="pt-2 font-inter-medium text-lg text-center">Você não realizou nenhum pedido ainda!</Text>
+        </View>
+        <View style={{ marginTop: 'auto' }} className="pb-4">
+            <ButtonLogin title="Novo pedido" onPress={ () => router.navigate("/home")}/>
+        </View>
+        
     </View>
     )
 }
