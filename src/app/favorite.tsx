01cc9f052274f4bd868388efb0fb07a1f19a4da9
Aleixopi15 <69539155+Aleixopi15@users.noreply.github.com>
2024-04-23 22:01:06 -0300

    Create nothing page fovorite and adjust nothing page history

--- src/app/favorite.tsx
+++ src/app/favorite.tsx
@@ -1,11 +1,27 @@
 import { View, Text, TouchableOpacity } from "react-native";
+import { FontAwesome5 } from "@expo/vector-icons";
+import { router } from "expo-router";
 
 import { Header } from "@/components/header";
+import { colors } from "@/styles/colors";
+import { ButtonLogin } from "@/components/button-login";
+
+
 
 export default function Favorite() {
     return(
     <View className="flex-1 bg-gray-100 p-8 pt-16">
         <Header title="Produtos favoritos" />
+
+        <View className="items-center justify-center">
+        <FontAwesome5 className="pt-32 opacity-50" name="heart" size={200} color={colors.gray[200]}/>
+        <Text className="font-inter-bold text-3xl">Nenhum item salvo</Text>
+        <Text className="pt-2 font-inter-medium text-lg text-center">Você ainda não favoritou nenhum produto!</Text>
+        </View>
+        <View style={{ marginTop: 'auto' }} className="pb-4">
+            <ButtonLogin title="Ver produtos" onPress={ () => router.navigate("/home")}/>
+        </View>
+        
     </View>
     )
 }
--- src/app/history.tsx
+++ src/app/history.tsx
@@ -1,12 +1,12 @@
 import { View, Text, TouchableOpacity } from "react-native";
 import { MaterialIcons } from "@expo/vector-icons";
+import { router } from "expo-router";
 
 import { Header } from "@/components/header";
 import { colors } from "@/styles/colors";
 import { ButtonLogin } from "@/components/button-login";
-import { Button } from "@/components/button";
-import { router } from "expo-router";
-colors
+
+
 
 export default function History() {
     return(
@@ -14,9 +14,9 @@ export default function History() {
         <Header title="Histórico de pedidos" />
 
         <View className="items-center justify-center">
-        <MaterialIcons className="pt-32 opacity-80" name="history" size={200} color={colors.gray[200]}/>
+        <MaterialIcons className="pt-32 opacity-50" name="history" size={200} color={colors.gray[200]}/>
         <Text className="font-inter-bold text-3xl">Nenhum pedido</Text>
-        <Text className="pt-2 font-inter-medium text-lg text-center">Você não realizou nenhum pedido ainda!</Text>
+        <Text className="pt-2 font-inter-medium text-lg text-center">Você ainda não realizou nenhum pedido!</Text>
         </View>
         <View style={{ marginTop: 'auto' }} className="pb-4">
             <ButtonLogin title="Novo pedido" onPress={ () => router.navigate("/home")}/>
